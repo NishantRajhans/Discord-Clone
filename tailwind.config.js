37e160f85bfb88e482ea1ecb4b2df84ef50cf25d
NishantRajhans <nishantrajhans35@gmail.com>
2023-02-04 14:04:20 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ['Open Sans', "ans-serif;"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "rgb(64 78 237)",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "rgb(246 246 246)",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "rgb(44 47 51)",
+      },
+    },
+  },
+  plugins: [],
+}
